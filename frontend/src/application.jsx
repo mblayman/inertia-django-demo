64a16510e8391b7549d63da9ea98935143cac70c
Matt Layman <matthewlayman@gmail.com>
2025-08-18 23:21:48 -0400

    Use the StrictMode component.

--- frontend/src/application.jsx
+++ frontend/src/application.jsx
@@ -1,4 +1,5 @@
 import { createInertiaApp } from '@inertiajs/react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Layout from './components/Layout'
 
@@ -10,6 +11,10 @@ createInertiaApp({
     return page
   },
   setup({ el, App, props }) {
-    createRoot(el).render(<App {...props} />)
+    createRoot(el).render(
+      <StrictMode>
+        <App {...props} />
+      </StrictMode>
+    )
   },
 })
